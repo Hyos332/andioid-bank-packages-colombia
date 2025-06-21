857346a61ae1852bff5eecf6b9a90118b2b5448f
Hyos332 <luis.hoyos1@alumnos.uneatlantico.es>
2025-06-21 17:17:43 +0200

    Agrege billeteras digitales en funcion private

--- Bancos.kt
+++ Bancos.kt
@@ -8,27 +8,41 @@ private val financialpackages = setof (
     "com.coinbase.android"
     "com.american.express.myca"
 
-    //top 20 colombia
+    //20 bancos de colombia
 
-    "co.com.bancolombia.personas.superapp"
-    "com.davivienda.daviviendaapp"
-    "com.bancodebogota.bancamovil"
-    "co.com.bbva.mb"
-    "com.grupoavaloc1.bancamovil"
-    "com.gnbsudmov"
-    "co.com.bancoagrario.icbanking"
-    "eu.netinfo.colpatria.system"
-    "com.grupoavalpo.bancamovil"
-    "com.co.app.unica.latam"
-    "com.bcs.retail"
-    "com.grupoavalav1.bancamovil"
-    "co.com.bancosantander.softtoken.prod"
-    "com.citi.mobile.cdbe"
-    "com.globalshares.mobile"
-    "co.com.bancofalabella.mobile.omc"
-    "se.accumulate.me.core.androidclient.csb"
-    "com.appmovil.app"
-    "com.yellowpepper.pichincha"
-    "com.nequi.MobileApp"
+    "co.com.bancolombia.personas.superapp",     // Bancolombia
+    "com.davivienda.daviviendaapp",             // Davivienda
+    "com.bancodebogota.bancamovil",             // Banco de Bogotá
+    "co.com.bbva.mb",                           // BBVA Colombia
+    "com.grupoavaloc1.bancamovil",              // Banco de Occidente
+    "com.gnbsudmov",                            // GNB Sudameris
+    "co.com.bancoagrario.icbanking",            // Banco Agrario
+    "eu.netinfo.colpatria.system",              // Scotiabank Colpatria
+    "com.grupoavalpo.bancamovil",               // Banco Popular
+    "com.co.app.unica.latam",                   // Itaú Colombia
+    "com.bcs.retail",                           // Banco Caja Social
+    "com.grupoavalav1.bancamovil",              // AV Villas
+    "co.com.bancosantander.softtoken.prod",     // Santander Colombia
+    "com.citi.mobile.cdbe",                     // Citibank
+    "com.globalshares.mobile",                  // GlobalShares (acciones, pero incluye finanzas , osea si.)
+    "co.com.bancofalabella.mobile.omc",         // Banco Falabella
+    "se.accumulate.me.core.androidclient.csb",  // Banco Coopcentral
+    "com.yellowpepper.pichincha",              // Banco Pichincha (otra versión)
+    "com.american.express.myca"                 // American Express
+)
 
+private val walletPackages = setOf(
+
+    //Billeteras digitales
+
+    "com.nequi.MobileApp",              // Nequi (Bancolombia)
+    "com.davivienda.daviplataapp",      // Daviplata (Davivienda)
+    "com.movilred.subscriber",          // MOVii
+    "com.rappi.bank",                   // RappiCuenta / RappiPay
+    "com.lulo.app",                     // Lulo Bank
+    "com.tpaga.wallet",                 // Tpaga
+    "com.uala.android",                 // Ualá
+    "com.mercadopago.wallet",           // Mercado Pago
+    "com.global66.mobile",              // Global66
+    "com.tuya.pay"                      // Tuya Pay
 )
